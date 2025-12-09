ba547f20d0edc183dfb1c942e204dd550c8c3520
傅一洪 <fuyihong.1@bytedance.com>
2025-12-09 10:58:54 +0800

    feat(chat): add api query_message_oauth_status and query_message_oauth_status

---
+++ hiagent-api/src/main/java/com/volcengine/hiagent/api/model/QueryAppMessageOauthStatusOpenRequest.java
@@ -0,0 +1,52 @@
+// Copyright (c) 2024 Bytedance Ltd. and/or its affiliates
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.volcengine.hiagent.api.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class QueryAppMessageOauthStatusOpenRequest {
+
+    @SerializedName("AppKey")
+    private String appKey;
+
+    @SerializedName("UserID")
+    private String userId;
+
+    @SerializedName("MessageID")
+    private String messageId;
+
+    public String getAppKey() {
+        return appKey;
+    }
+
+    public void setAppKey(String appKey) {
+        this.appKey = appKey;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+}
